8f6ad9e49513fe92a678f9e7c7cc34293fefa46b
S.M. Jagadish <smjagadish@gmail.com>
2025-04-11 22:36:28 -0400

    added rest controller and updated app start

--- supplychain/src/main/java/org/example/appStart.java
+++ supplychain/src/main/java/org/example/appStart.java
@@ -6,8 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.controller")
 public class appStart {
     private static final Logger logger = LoggerFactory.getLogger(appStart.class);
     public static void main(String[] args) {
