a4fbf49c13e36171067a6a6c8416a54b63db5ef7
S.M. Jagadish <smjagadish@gmail.com>
2025-04-11 21:19:15 -0400

    spring boot skeleton app for supply chain poc

---
+++ supplychain/src/main/java/org/example/appStart.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import javafx.application.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class appStart {
+    private static final Logger logger = LoggerFactory.getLogger(appStart.class);
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(appStart.class, args);
+        logger.info("Application started successfully");
+    }
+}
